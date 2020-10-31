5e0347592782941fc03047e95a60b2ab27fbe5e7
kaylinpham <kaylinpham109@gmail.com>
2020-10-31 19:24:37 +0700

    ignore node

--- TodoApp/webpack.config.js
+++ TodoApp/webpack.config.js
@@ -1,18 +1,20 @@
 var path = require('path');
 module.exports = {
     mode: "development",
-    entry: path.join(__dirname, 'src', 'index.js'),
+    entry: path.join(__dirname, 'src', 'script.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'script.js'
+        filename: 'index.js'
     },
     module: {
         rules: [{
+
             test: /\\.jsx?$/,
             loader: 'babel-loader',
             options: {
                 presets: ['@babel/present-env']
             }
+
         }]
     },
     stats: {
